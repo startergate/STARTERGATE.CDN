de0b4b0293241a806ffd7df8585f767e6bafcd5a
STARTERGATE <32726809+startergate@users.noreply.github.com>
2019-04-30 11:01:22 +0900

    엄로드 기본 코드
    아톰이 알아서 고친거 + 엄로드 기본 코드

--- routes/profile.js
+++ routes/profile.js
@@ -17,4 +17,14 @@ router.get('/img/:id', (req, res, next) => {
   });
 });
 
+router.post('/img/:id/upload', (req, res, next) => {
+  fs.writeFile('public/images/sid/profile/' + req.params.id + '.png', req.body, err => {
+    if (err) {
+      console.log(err);
+      res.sendStatus(500);
+      return;
+    }
+  });
+});
+
 module.exports = router;
