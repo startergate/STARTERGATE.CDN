755a818d410ee7a8584963f277f0dadc501393f0
STARTERGATE <32726809+startergate@users.noreply.github.com>
2019-05-08 21:19:49 +0900

    Check IP

--- app.js
+++ app.js
@@ -24,6 +24,8 @@ app.use(express.urlencoded({
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.enable('trust proxy');
+
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
--- routes/bot.js
+++ routes/bot.js
@@ -9,9 +9,11 @@ const ds_token = JSON.parse(fs.readFileSync('private/data/discord/bot.json'));
 /* GET home page. */
 router.get('/token/:id', (req, res, next) => {
   if (ds_token.data[req.params.id]) {
+    if (req.ip in ds_token.data[req.params.id].allowed_ip || req.ip === '::1') {
       res.set('Content-Type', 'text/plain');
       res.send(ds_token.data[req.params.id].discord_token.toString());
       return;
+    }
   }
   res.sendStatus(404);
 });
